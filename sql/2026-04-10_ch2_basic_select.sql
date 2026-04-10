7fa78349fde0a8410641c30d0b1c7e0b3ef0d1e1
dzm75277229 <duzhimei0728@163.com>
2026-04-10 16:19:28 +0800

    sql

--- sql/2026-04-10_ch2_basic_select.sql
+++ sql/2026-04-10_ch2_basic_select.sql
@@ -1,9 +1,15 @@
+#创建数据库
+CREATE DATABASE test;
+#删除数据库
+DROP DATABASE test;
 #显示所有的数据库
 SHOW DATABASES;
 #显示表
 SHOW TABLES;
 #显示表结构
 SHOW COLUMNS FROM gongsiname;
+#删除表
+
 #按照降序进行排序
 select * from gongsiname order by id desc;
 #按照多行降序进行排序
@@ -33,5 +39,17 @@ select * from gongsiname where name REGEXP '[ei]vo';
 select * from gongsiname where name REGEXP '[0-9]vo';
 select * from gongsiname where name REGEXP '.';
 select * from gongsiname where name REGEXP '\\.';
-#使用函数
+#使用函数,count()计算出多少行非空，sum(不可以为*)
 select name,UPPER(name) AS name_upper from gongsiname;
+SELECT COUNT(*) FROM gongsiname;
+SELECT COUNT(price) FROM gongsiname;
+SELECT SUM(price) FROM gongsiname;
+SELECT AVG(price) FROM gongsiname;
+SELECT MAX(price) FROM gongsiname;
+SELECT MIN(price) FROM gongsiname;
+#group by,出现在select后面的列只能是groupby后面的列和聚合函数，where在groupby前，having在groupby后
+SELECT GP,COUNT(*) FROM gongsiname GROUP BY GP;
+SELECT GP,COUNT(*) FROM gongsiname where price=123 GROUP BY GP;
+SELECT GP,COUNT(*) FROM gongsiname GROUP BY GP,name;
+#where在分组前过滤，having在分组之后过滤，只有orderby可以使用别名，其他尽量不使用
+UPDATE gongsiname SET price=4 WHERE name='红米';
